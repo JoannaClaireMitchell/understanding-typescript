a3a5d6089de35e675c752e00b0ae971a67204422
Joanna Mitchell <joannaclairemitchell@gmail.com>
2023-03-21 14:18:41 +0100

    add advanced types

--- src/basics.ts
+++ src/basics.ts
@@ -1,10 +1,15 @@
-function add(num1: number, num2: number, showResult: boolean, phrase: string){
-  const result = num1 + num2
-  if(showResult){
-    console.log(phrase + result)
-  }else{
-    return result
+function addNum(
+  num1: number,
+  num2: number,
+  showResult: boolean,
+  phrase: string
+) {
+  const result = num1 + num2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return result;
   }
 }
 
-add(5, 3.7, true, 'Result is: ')
+addNum(5, 3.7, true, "Result is: ");
